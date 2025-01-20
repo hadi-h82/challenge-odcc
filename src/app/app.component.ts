438fee074c147e9da79547e482cd5d3a5a270b26
hadi <hadi.deris.h82@gmail.com>
2025-01-21 00:06:40 +0330

    add three button for CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'challenge-odcc';
+  isPopupVisible = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addUser() {
+    console.log('addUser');
+  }
+
+  onEditClick = (e: DxDataGridTypes.ColumnButtonClickEvent) => {
+    console.log('edit');
+    // console.log(e);
+  };
+
+  onDeleteClick = (e: DxDataGridTypes.ColumnButtonClickEvent) => {
+    console.log('delete');
+    // console.log(e);
+  };
 }
